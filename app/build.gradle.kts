d5706ecf4184c7bb9c5330ea35b53159d1fe8dd3
Claus Polanka <claus.polanka@erstegroup.com>
2024-12-22 17:38:12 +0100

    org.springframework:spring-test [6.2.0 -> 6.2.1]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     implementation("org.hibernate:hibernate-core:7.0.0.Beta3")
     implementation("org.springframework.data:spring-data-jpa:3.4.0")
     runtimeOnly("org.postgresql:postgresql:42.7.3")
-    testImplementation("org.springframework:spring-test:6.2.0")
+    testImplementation("org.springframework:spring-test:6.2.1")
     testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
 }
 
